6b5f805caf5a5257c5b561a3bd3739fd28eb69c5
tziesse <78933586+tziesse@users.noreply.github.com>
2021-11-11 20:36:52 -0500

    Add files via upload
    Question 2

--- Question 2/main.c
+++ Question 2/main.c
@@ -1,1 +1,18 @@
-sd
+//question 2
+#include "msp430G2553.h"
+
+void main(void)
+{
+    WDTCTL = WDTPW + WDTHOLD;  // Stop WDT
+     P1DIR |= BIT6;             // P1.6 set for output
+     P1OUT = 0x00;
+     P1SEL |= BIT6;             // select TA0.1 output signal
+     TACCR0 = 62500-1;             // PWM Time Period/ frequency
+     TACCTL1 = OUTMOD_7;          // reset/set mode 7 for output signal
+     TACCR1 = 6250-1;                // PWM Duty cycle is 10%
+     TACTL = TASSEL_2 + ID_3 + MC_1;   // SMCLK and Up Mode,
+
+     while(1){
+         P1OUT ^= BIT6; //display on led and on pin 1.6
+     }
+}
