21727e7f943d8cb79d4a48c0f5668c5ca928c27e
tziesse <78933586+tziesse@users.noreply.github.com>
2021-11-11 20:36:31 -0500

    Add files via upload
    Question 1 code

--- Question 1/main.c
+++ Question 1/main.c
@@ -1,1 +1,21 @@
-sdssd
+//question 1
+#include <msp430.h>
+#define RedLED BIT6
+#define GreenLED BIT0
+#define ToggleLeds (P1OUT ^= RedLED|GreenLED)
+#define Button BIT3
+void main(void)
+{
+BCSCTL2 |= DIVS_3;
+WDTCTL = WDT_MDLY_32;
+IE1 |= WDTIE;
+P1DIR = RedLED|GreenLED;
+P1OUT = RedLED;
+_enable_interrupts();
+LPM1;
+}
+#pragma vector = WDT_VECTOR
+__interrupt void WDT(void)
+{
+P1OUT = ToggleLeds;
+}
